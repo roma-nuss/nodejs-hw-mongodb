6b77e1b199f028752c2088954c639a772e6918b2
Roman <romka22830@gmail.com>
2025-01-18 20:25:48 +0200

    update

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -1,29 +1,24 @@
+// src/controllers/contactsController.js
 import { findAllContacts, findContactById } from '../services/contacts.js';
 
+// Получить все контакты
 export const getContacts = async (req, res) => {
   try {
     const contacts = await findAllContacts();
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
+    res.json(contacts);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
+// Получить контакт по ID
 export const getContactById = async (req, res) => {
   try {
     const contact = await findContactById(req.params.contactId);
     if (!contact) {
       return res.status(404).json({ message: 'Contact not found' });
     }
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${req.params.contactId}!`,
-      data: contact,
-    });
+    res.json(contact);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
