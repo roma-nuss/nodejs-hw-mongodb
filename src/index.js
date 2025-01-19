67d6523786530dbb8231412a89422982b0fed56f
Roman <romka22830@gmail.com>
2025-01-19 16:16:54 +0200

    update

--- src/index.js
+++ src/index.js
@@ -6,17 +6,13 @@ import contactsRoutes from './routes/contactsRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(express.json()); // Поддержка JSON в body запросов
+app.use(express.json());
 
-// Подключаем маршруты для контактов (без '/api')
 app.use('/contacts', contactsRoutes);
 
-// Обработка некорректных маршрутов
 app.use((req, res) => {
   res.status(404).json({
-    status: 404,
     message: 'Not Found',
-    data: null,
   });
 });
 
