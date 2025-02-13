928aba1687e057d12efba3c652f261f39e42cebd
Roman <romka22830@gmail.com>
2025-02-13 02:42:10 +0200

    update

--- src/validators/contactValidators.js
+++ src/validators/contactValidators.js
@@ -1,4 +1,4 @@
-//src/validators/contactValidators.js
+// src/validators/contactValidators.js
 import Joi from 'joi';
 
 export const contactSchema = Joi.object({
@@ -8,7 +8,7 @@ export const contactSchema = Joi.object({
     .required(),
   email: Joi.string().email(),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid('personal', 'business').required(),
+  contactType: Joi.string().valid('work', 'home', 'personal').required(), // Обновлено на work, home, personal
 });
 
 export const updateContactSchema = Joi.object({
@@ -16,5 +16,5 @@ export const updateContactSchema = Joi.object({
   phoneNumber: Joi.string().pattern(/^\+?\d{10,15}$/),
   email: Joi.string().email(),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid('personal', 'business'),
+  contactType: Joi.string().valid('work', 'home', 'personal'), // Обновлено на work, home, personal
 }).or('name', 'phoneNumber', 'email', 'isFavourite', 'contactType');
